7661e3142be806dc3de1a3b369502dddb5c9341e
Ahmad Ammar Musyaffa <ahmad.ammar.2205356@students.um.ac.id>
2024-01-22 20:25:28 +0700

    fixing content

--- js/script.js
+++ js/script.js
@@ -1,16 +1,15 @@
+// Loading
 const preloader = document.querySelector(".preloader");
 
-// Function to remove the preloader after 2 seconds
 function removePreloader() {
     preloader.remove();
 }
 
-// Wait for the window to load
 window.addEventListener('load', () => {
-    // Set a timeout of 2000 milliseconds (2 seconds) before removing the preloader
     setTimeout(removePreloader, 3000);
 });
 
+// Carousel
 document.addEventListener('DOMContentLoaded', function () {
     const carousel = document.querySelector('.carousel');
     const leftArrow = document.querySelector('.arrow.left');
