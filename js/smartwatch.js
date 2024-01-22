623cacfd2d0e3c8a37f08003483f571054e1c5da
Ahmad Ammar Musyaffa <ahmad.ammar.2205356@students.um.ac.id>
2024-01-22 19:18:09 +0700

    adding furniture, fitness, and smartwatch path

--- js/smartwatch.js
+++ js/smartwatch.js
@@ -1,15 +1,8 @@
 const preloader = document.querySelector(".preloader");
 
-// Function to remove the preloader after 2 seconds
-function removePreloader() {
-    preloader.remove();
-}
-
-// Wait for the window to load
 window.addEventListener('load', () => {
-    // Set a timeout of 2000 milliseconds (2 seconds) before removing the preloader
-    setTimeout(removePreloader, 3000);
-});
+    preloader.remove();
+})
 
 document.addEventListener('DOMContentLoaded', function () {
     const carousel = document.querySelector('.carousel');
